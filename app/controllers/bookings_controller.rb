919f62bd4a87224b5448122da8969a9dfd26f03f
jerome dharveng <jerome.dharveng@hotmail.com>
2018-05-29 14:13:19 +0100

    create method show in bookingController to test the new/ create from the booking Controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,30 @@
+class BookingsController < ApplicationController
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:review, :rating)
+  end
+end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'books/index'
-  devise_for :users do
-    resources :bookings
-  end
+  devise_for :users
+  resources :bookings
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
