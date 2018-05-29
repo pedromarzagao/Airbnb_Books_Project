ad19cb47c18c6d4b0b011f2444b002f28ff7449c
jerome dharveng <jerome.dharveng@hotmail.com>
2018-05-29 17:02:48 +0100

    migration latititude and longitude in Book model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_142724) do
+ActiveRecord::Schema.define(version: 2018_05_29_160004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2018_05_29_142724) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
