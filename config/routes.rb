d2816eaeb0e186f801ebe5b50137c38263e2c7b3
jerome dharveng <jerome.dharveng@hotmail.com>
2018-05-29 12:39:23 +0100

    Add nested routed bookings inside devise_for :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'books/index'
-  devise_for :users
+  devise_for :users do
+    resources :bookings
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
