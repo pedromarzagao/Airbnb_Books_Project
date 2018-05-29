dc92310d7124f195ccff2f731ac66c2cc0a78aa0
Pedro Marzagao <pedromarzagao@gmail.com>
2018-05-29 11:11:55 +0100

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2018_05_28_162320) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "summary"
-    t.integer "rating"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
