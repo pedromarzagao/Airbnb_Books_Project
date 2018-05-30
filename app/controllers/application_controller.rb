e0887a1167a5c3479085eacaf4abc95696bac010
Pedro Marzagao <pedromarzagao@gmail.com>
2018-05-30 14:11:57 +0100

    pictures from cloudinary added to seed and image tags for users and books added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :picture, :password])
+
+  end
+
 
 end
