f3421e107471da853ce6453ed4f06597f956e2ce
jerome dharveng <jerome.dharveng@hotmail.com>
2018-05-30 15:49:40 +0100

    JS setup for flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,14 @@
 import "bootstrap";
-@import 'flatpickr/dist/flatpickr.css';
+import './application.css';
+import initMap from '../components/map';
+import initDate from '../components/datepickr'
+
+
+
+const mapElement = document.getElementById('map');
+initMap(mapElement);
+
+const startDateinput = document.getElementById('crawl_start_date');
+const endDateinput = document.getElementById('crawl_end_date');
+initDate(startDateinput, endDateinput);
+
