a09829eb8332f767d06871b93f9ead60e5cdd61b
jerome dharveng <jerome.dharveng@hotmail.com>
2018-05-31 15:17:18 +0100

    End of part2 calc duration

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  validates :date_begin, presence: true
+  validates :date_end, presence: true
   validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
